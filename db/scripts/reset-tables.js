b42b13a28075b6956101589e420a59e0e3f26f53
Alexander Carr <1alexander.carr@gmail.com>
2023-10-25 15:04:43 +0100

    reset database populated with more rows, tested and elephantSQL checked

--- db/scripts/reset-tables.js
+++ db/scripts/reset-tables.js
@@ -6,7 +6,20 @@ try {
   const insertedRows = await resetAllTables([
     { week: "Week 1", subject: "Mindset", title: "Computational Thinking", resource: "https://www.youtube.com/watch?v=qbnTZCj0ugI" },
     { week: "Week 1", subject: "Mindset", title: "Agile Methodology", resource: "https://www.youtube.com/watch?v=ZZ_vnqvW4DQ" },
-    { week: "Week 2", subject: "HTML", title: "Tags", resource: "https://www.youtube.com/watch?v=tv6bxtCjqDI" }
+    { week: "Week 2", subject: "HTML", title: "Tags", resource: "https://www.youtube.com/watch?v=tv6bxtCjqDI" },
+    { week: "Week 2", subject: "HTML", title: "Semantics", resource: "https://www.youtube.com/watch?v=abc123" },
+    { week: "Week 3", subject: "CSS", title: "Selectors", resource: "https://www.youtube.com/watch?v=xyz456" },
+    { week: "Week 3", subject: "CSS", title: "Box Model", resource: "https://www.youtube.com/watch?v=def789" },
+    { week: "Week 4", subject: "JavaScript", title: "Variables", resource: "https://www.youtube.com/watch?v=ghi101" },
+    { week: "Week 4", subject: "JavaScript", title: "Functions", resource: "https://www.youtube.com/watch?v=jklmno" },
+    { week: "Week 5", subject: "Mindset", title: "Growth Mindset", resource: "https://www.youtube.com/watch?v=mnopqr" },
+    { week: "Week 5", subject: "Mindset", title: "Resilience", resource: "https://www.youtube.com/watch?v=stuvwx" },
+    { week: "Week 6", subject: "Node", title: "Introduction to Node.js", resource: "https://www.youtube.com/watch?v=uvwxzy" },
+    { week: "Week 6", subject: "Node", title: "Express.js", resource: "https://www.youtube.com/watch?v=ijklmn" },
+    { week: "Week 7", subject: "HTML", title: "Forms", resource: "https://www.youtube.com/watch?v=pqrstu" },
+    { week: "Week 7", subject: "HTML", title: "Accessibility", resource: "https://www.youtube.com/watch?v=vwxyz1" },
+    { week: "Week 8", subject: "CSS", title: "Flexbox", resource: "https://www.youtube.com/watch?v=def789" },
+    { week: "Week 8", subject: "CSS", title: "Grid", resource: "https://www.youtube.com/watch?v=jklmno" }
   ]);
   console.log("Reset all tables and inserted data");
   console.log(insertedRows);
