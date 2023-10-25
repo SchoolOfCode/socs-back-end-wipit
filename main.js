a72b28ef7e9cfbf119730b5d1271a5080ada7b21
Yogiz88 <m.z.zawadzkimichal@gmail.com>
2023-10-25 12:19:15 +0100

    main

---
+++ main.js
@@ -0,0 +1,12 @@
+// This file contains any logic that needs to run once when the page loads.
+
+import { getWeekResources } from "./resources/resources.controller";
+
+
+/* 
+1) Select button from index.html file
+2) Add EventListener(Click) to this button
+3) write function to retrive data from DB 
+4) Write function to display this data on the page (select div where data will be displayed)
+
+*/
