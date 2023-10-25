de5cdc38278825b68998cf5d99b59a6b3010f650
Alexander Carr <1alexander.carr@gmail.com>
2023-10-25 15:57:02 +0100

    duplicated week 1 button function for week 2

--- resources/resources.controller.js
+++ resources/resources.controller.js
@@ -6,3 +6,10 @@ export async function getWeekResources() {
   );
   return data.rows;
 }
+
+export async function getWeekTwoResources() {
+  const data = await pool.query(
+    "SELECT * FROM resource_library WHERE week = 'Week 2';"
+  );
+  return data.rows;
+}
--- resources/resources.router.js
+++ resources/resources.router.js
@@ -1,10 +1,16 @@
 import express from "express";
 const router = express.Router();
 
-import { getWeekResources } from "./resources.controller.js";
+import { getWeekResources, getWeekTwoResources } from "./resources.controller.js";
 
+// rename route path to /week1 once everything else is completed
 router.get("/", async function (_req, res) {
   const result = await getWeekResources();
+  res.json({ success: true, payload: result });
+});
+
+router.get("/week2", async function (_req, res) {
+  const result = await getWeekTwoResources();
   res.json({ success: true, payload: result });
 });
 
