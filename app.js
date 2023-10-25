e75c63b446696588bc09c626233aa4fdaddfd719
mickeymarse <mmserrapica@tutanota.com>
2023-10-25 14:27:48 +0100

    Added cors package and imported it.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // import dependencies required to create server
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import resourcesRouter from "./resources/resources.router.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static("public"));
