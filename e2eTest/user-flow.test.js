41d2a317b77273a40b05d9800eaff904cd525c1d
mickeymarse <mmserrapica@tutanota.com>
2023-10-26 16:09:57 +0100

    Testing Codegen

--- e2eTest/user-flow.test.js
+++ e2eTest/user-flow.test.js
@@ -5,21 +5,30 @@ test.beforeEach(async () => {
   await resetAllTables();
 });
 
+// test("User flow for SOCS", async ({ page }) => {
+//   await page.goto("http://localhost:5500/public");
+//   await expect(
+//     page.getByRole("heading", { name: "School of Code Stash" })
+//   ).toBeVisible();
+//   //   await page.getByLabel("Name").fill("c4rm314");
+//   //   await expect(page.getByLabel("Name")).toHaveValue(/c4rm314/);
+//   //   await page
+//   //     .getByLabel("Tweet")
+//   //     .fill("Went to a fundraiser last night and it was lovely!");
+//   //   await expect(page.getByLabel("Tweet")).toHaveValue(
+//   //     /Went to a fundraiser last night and it was lovely!/
+//   //   );
+//   //   await page.getByRole("button", { name: /Send!/ }).click();
+//   //   await expect(page.getByLabel("Name")).toHaveValue("");
+//   //   await expect(page.getByLabel("Tweet")).toHaveValue("");
+//   //   await expect(
+//   //     page.getByText("c4rm314 Went to a fundraiser last night and it was lovely!")
+//   //   ).toBeVisible();
+// });
+
 test("User flow for SOCS", async ({ page }) => {
-  //   await page.goto("http://localhost:5500");
-  //   await expect(page.getByRole("heading", { name: "TestTer" })).toBeVisible();
-  //   await page.getByLabel("Name").fill("c4rm314");
-  //   await expect(page.getByLabel("Name")).toHaveValue(/c4rm314/);
-  //   await page
-  //     .getByLabel("Tweet")
-  //     .fill("Went to a fundraiser last night and it was lovely!");
-  //   await expect(page.getByLabel("Tweet")).toHaveValue(
-  //     /Went to a fundraiser last night and it was lovely!/
-  //   );
-  //   await page.getByRole("button", { name: /Send!/ }).click();
-  //   await expect(page.getByLabel("Name")).toHaveValue("");
-  //   await expect(page.getByLabel("Tweet")).toHaveValue("");
-  //   await expect(
-  //     page.getByText("c4rm314 Went to a fundraiser last night and it was lovely!")
-  //   ).toBeVisible();
+  await page.goto("http://localhost:5500/public/");
+  await page.getByRole("link", { name: " HOME" }).click();
+  await page.getByRole("link", { name: "Resources by Week" }).click();
+  await page.getByRole("button", { name: "Week 1", exact: true }).click();
 });
