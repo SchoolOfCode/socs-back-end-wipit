da4957aa5852d95c17530d68ac4231efd8996a77
mickeymarse <mmserrapica@tutanota.com>
2023-11-02 09:06:02 +0000

    Tried to change cors

--- app.js
+++ app.js
@@ -7,19 +7,10 @@ import resourcesRouter from "./resources/resources.router.js";
 
 const app = express();
 
-// Specified port that frontend will use to connect to backend api, allowed to use specified methods
-// app.use(
-//   cors({
-//     origin: "http://127.0.0.1:5500",
-//     methods: ["GET", "POST", "DELETE", "PATCH"],
-//   })
-// );
 app.use(cors());
 app.use(morgan("dev"));
 // takes requests and changes them to JSON format
 app.use(express.json());
-// call frontend as a static website within the public folder
-// app.use(express.static("public"));
 // define route for api
 app.use("/api/resources", resourcesRouter);
 
